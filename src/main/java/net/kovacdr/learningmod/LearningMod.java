6dbe9c8fde2b99564ea8e69e3ff683dda2b8fef3
Kovac <kovaski33@gmail.com>
2026-02-09 21:03:04 -0500

    feat: Added bismuth_block and bismuth_ore
    - Added bimsuth_block and bismuth_ore texture

--- src/main/java/net/kovacdr/learningmod/LearningMod.java
+++ src/main/java/net/kovacdr/learningmod/LearningMod.java
@@ -1,5 +1,6 @@
 package net.kovacdr.learningmod;
 
+import net.kovacdr.learningmod.block.ModBlocks;
 import net.kovacdr.learningmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -39,6 +40,7 @@ public class LearningMod {
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -56,6 +58,11 @@ public class LearningMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.BISMUTH);
             event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
     }
 
